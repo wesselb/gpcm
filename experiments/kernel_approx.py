d8677c4774c71a18edfb8ca9fe27dd165eb07afc
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-01-09 12:46:52 +0000

    Add sampling script

---
+++ experiments/kernel_approx.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from lab import B
+import wbml.plot
+import wbml.out
+
+from gpcm.gprv.kernel_approx import kernel_approx_u, ku
+
+B.epsilon = 1e-8
+
+# Define some test parameters.
+lam = 1/2  # Model length scale
+wbml.out.kv('Lambda', lam)
+
+# Set window to twice the length scale of the model.
+a = lam/2
+wbml.out.kv('Window length scale', 1/a)
+wbml.out.kv('Alpha', a)
+
+# Make the prior kernel have variance one.
+at = (2*a)**.5
+
+t = np.linspace(0, 10, 200)
+tu = np.linspace(0, 2/a, 20)
+
+g = 1/(4*(tu[1] - tu[0]))  # Kernel inter-domain transformation
+wbml.out.kv('Gamma', g)
+gt = (2*g)**.5
+
+noise_f = np.random.randn(len(t), 1)
+ks, fs = [], []
+with wbml.out.Progress(name='Sampling', total=5) as progress:
+    for i in range(5):
+        progress()
+
+        f, K = None, None
+
+        # Sample random u.
+        while f is None:
+            try:
+                Ku = ku(gt, g, tu[:, None], tu[None, :])
+                u = B.matmul(B.cholesky(Ku), np.random.randn(len(tu), 1))[:, 0]
+
+                # Construct the kernel matrix.
+                K = B.reg(kernel_approx_u(at, a, gt, g, lam, t, t, tu, u))
+                wbml.out.kv('Minimal eigenvalue', min(np.linalg.eigvals(K)))
+                wbml.out.kv('Sampled variance', K[0, 0])
+                K /= K[0, 0]  # Set to unity variance.
+
+                # Draw sample function.
+                f = B.matmul(B.cholesky(K), noise_f)[:, 0]
+            except np.linalg.LinAlgError:
+                wbml.out.out('Sampling failed. Trying again...')
+                continue
+
+        fs.append(f)
+        ks.append(K[0, :])
+
+# Compute PSDs.
+psds = []
+n_zero = 1000
+for k in ks:
+    k = B.concat(k, B.zeros(n_zero))
+    k_symmetric = B.concat(k, k[1:-1][::-1])
+    psd = np.fft.fft(k_symmetric)
+    freqs = np.fft.fftfreq(len(psd))/(t[1] - t[0])
+
+    # Should be real and positive, but the numerics may not be in our favour.
+    psd = np.abs(np.real(psd))
+
+    # Now scale appropriately.
+    total_power = np.sum(psd*np.abs(freqs))
+    psd /= total_power/k[0]
+
+    # Convert to dB.
+    psd = 10*np.log10(psd)
+
+    psds.append(psd)
+
+fs = np.stack(fs).T
+ks = np.stack(ks).T
+psds = np.stack(psds).T
+
+# Plot.
+plt.figure(figsize=(15, 3))
+
+plt.subplot(1, 2, 1)
+plt.plot(t, fs, lw=1)
+plt.title('Function')
+plt.xlabel('Time (s)')
+wbml.plot.tweak(legend=False)
+
+plt.subplot(1, 4, 3)
+plt.plot(t, ks, lw=1)
+plt.scatter(tu, tu*0, s=5, marker='o', c='black')
+plt.title('Kernel')
+plt.xlabel('Lag (s)')
+wbml.plot.tweak(legend=False)
+
+plt.subplot(1, 4, 4)
+plt.title('PSD (dB)')
+inds = np.arange(int(len(freqs)/2))
+inds = inds[freqs[inds] <= 1]
+plt.plot(freqs[inds], psds[inds, :], lw=1)
+plt.xlabel('Frequency (Hz)')
+plt.ylim(-40, 0)
+wbml.plot.tweak(legend=False)
+
+plt.tight_layout()
+plt.show()
