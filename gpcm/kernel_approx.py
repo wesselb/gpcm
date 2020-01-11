4c342d04799a160608b07d353487b38a89a40536
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-01-11 16:49:36 +0000

    Clean up kernel approximation

--- gpcm/kernel_approx.py
+++ gpcm/kernel_approx.py
@@ -1,21 +1,6 @@
 import lab as B
-from matrix import Dense
 
-__all__ = ['K_u', 'kernel_approx_u']
-
-
-def K_u(model):
-    """Covariance matrix :math:`K_u` of :math:`u(t)` at inputs `t_u`.
-
-    Args:
-        model (:class:`.model.GPRV`): Model.
-
-    Returns:
-        matrix: :math:`K_u` of shape `(len(model.t_u), len(model.t_u))`.
-    """
-
-    t_u = B.flatten(model.t_u)
-    return Dense(model.k_u(t_u[None, :], t_u[:, None]))
+__all__ = ['kernel_approx_u']
 
 
 def kernel_approx_u(model, t1, t2, u):
@@ -36,13 +21,10 @@ def kernel_approx_u(model, t1, t2, u):
     part1 = model.i_hx(t1[:, None], t2[None, :])
 
     # Construct the second part.
-    Lu = B.cholesky(K_u(model))
+    Lu = B.cholesky(model.K_u())
     iLu = B.dense(B.trisolve(Lu, B.eye(Lu)))
     iLu_u = B.mm(iLu, u[:, None])
-    Kiux = model.i_ux(t1[:, None, None, None],
-                      t2[None, :, None, None],
-                      model.t_u[None, None, :, None],
-                      model.t_u[None, None, None, :])
+    Kiux = model.I_ux(t1, t2)
     trisolved = B.mm(B.mm(iLu, Kiux, tr_a=True), iLu)
     part2 = B.trace(trisolved, axis1=2, axis2=3) - \
             B.trace(B.mm(B.mm(iLu_u, trisolved, tr_a=True), iLu_u),
