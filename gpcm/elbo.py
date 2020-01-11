11bfe250ee196c2ea74949f61e8a2720000e269e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-01-11 16:49:14 +0000

    Clean up ELBO

--- gpcm/elbo.py
+++ gpcm/elbo.py
@@ -1,11 +1,17 @@
 import lab as B
 import numpy as np
+import wbml.out
+from matrix import Dense
 from stheno import Normal
 
-from .kernel_approx import kernel_approx_u, K_u
+from .kernel_approx import kernel_approx_u
 from .util import collect, pd_inv
 
-__all__ = ['construct', 'predict', 'predict_kernel', 'predict_fourier']
+__all__ = ['construct',
+           'elbo',
+           'predict',
+           'predict_kernel',
+           'predict_fourier']
 
 
 def construct(model, t, y, sigma, mu_u, cov_u):
@@ -23,39 +29,36 @@ def construct(model, t, y, sigma, mu_u, cov_u):
     Returns:
         :class:`collections.namedtuple`: Computed quantities.
     """
-
     n = B.length(t)
     q_u = Normal(cov_u, mu_u)
 
-    I_hx_ = model.i_hx(t, t)
-    I_ux_ = model.I_ux()  # Does not depend on `t`!
-    I_hz_ = model.I_hz(t)
-    I_uz_ = model.I_uz(t)
-
-    # Put data dimensions first.
-    I_uz_ = B.transpose(I_uz_, perm=(2, 0, 1))
-    I_hz_ = B.transpose(I_hz_, perm=(2, 0, 1))
+    # Construct integrals.
+    I_hx = model.i_hx(t, t)
+    I_ux = model.I_ux()
+    I_hz = model.I_hz(t)
+    I_uz = model.I_uz(t)
 
     # Construct kernel matrices.
-    K_z_ = model.K_z()
-    K_z_inv = pd_inv(K_z_)
-    K_u_ = K_u(model)
-    L_u = B.chol(K_u_)
-    K_u_inv = pd_inv(K_u_)
+    K_z = model.K_z()
+    K_z_inv = pd_inv(K_z)
+
+    K_u = model.K_u()
+    K_u_inv = pd_inv(K_u)
+    L_u = B.chol(K_u)  # Cholesky will be cached.
 
     # Do some precomputations.
     L_u_tiled = B.tile(L_u[None, :, :], n, 1, 1)
-    L_u_inv_I_uz_ = B.trisolve(L_u_tiled, I_uz_)
-    I_zu_inv_K_u_I_uz = B.mm(L_u_inv_I_uz_, L_u_inv_I_uz_, tr_a=True)
+    L_u_inv_I_uz = B.trisolve(L_u_tiled, I_uz)
+    I_zu_inv_K_u_I_uz = B.mm(L_u_inv_I_uz, L_u_inv_I_uz, tr_a=True)
     I_zu_inv_K_u_I_uz_sum = B.sum(I_zu_inv_K_u_I_uz, axis=0)
 
-    I_hx_sum = B.sum(I_hx_, axis=0)
-    I_ux_sum = n*I_ux_  # Independent of `t`!
-    I_hz_sum = B.sum(I_hz_, axis=0)
-    I_uz_sum = B.sum(y[:, None, None]*I_uz_, axis=0)  # Weighted by data.
+    I_hx_sum = B.sum(I_hx, axis=0)
+    I_hz_sum = B.sum(I_hz, axis=0)
+    I_ux_sum = n*I_ux
+    I_uz_sum = B.sum(y[:, None, None]*I_uz, axis=0)  # Weighted by data.
 
-    A_sum = I_ux_sum - B.sum(B.mm(B.mm(I_uz_, B.dense(K_z_inv)),
-                                  I_uz_, tr_b=True), axis=0)
+    A_sum = I_ux_sum - \
+            B.sum(B.mm(I_uz, B.dense(K_z_inv), I_uz, tr_c=True), axis=0)
     B_sum = I_hz_sum - I_zu_inv_K_u_I_uz_sum
     c_sum = I_hx_sum - \
             B.sum(K_u_inv*I_ux_sum) - \
@@ -63,15 +66,14 @@ def construct(model, t, y, sigma, mu_u, cov_u):
             B.sum(K_z_inv*I_zu_inv_K_u_I_uz_sum)
 
     # Compute optimal q(z).
-    inv_cov_z = K_z_ + 1/sigma**2* \
-                B.sum(B_sum + B.mm(B.mm(I_uz_, B.dense(q_u.m2), tr_a=True),
-                                   I_uz_),
-                      axis=0)
-    inv_cov_z_mu_z = 1/sigma**2*B.mm(I_uz_sum, mu_u, tr_a=True)
-    L_inv_cov_z = B.chol(B.reg(inv_cov_z))
+    inv_cov_z = \
+        K_z + 1/sigma**2* \
+        B.sum(B_sum + B.mm(I_uz, B.dense(q_u.m2), I_uz, tr_a=True), axis=0)
+    inv_cov_z_mu_z = 1/sigma**2*B.mm(I_uz_sum, q_u.mean, tr_a=True)
+    L_inv_cov_z = B.chol(Dense(inv_cov_z))
     root = B.trisolve(L_inv_cov_z, inv_cov_z_mu_z)
 
-    # TODO: Optimise this!
+    # Construct prior p(u).
     p_u = Normal(K_u_inv)
 
     return collect(model=model,
@@ -81,16 +83,16 @@ def construct(model, t, y, sigma, mu_u, cov_u):
                    y=y,
                    sigma=sigma,
 
-                   K_z_=K_z_,
+                   K_z=K_z,
                    K_z_inv=K_z_inv,
 
-                   K_u_=K_u_,
+                   K_u=K_u,
                    K_u_inv=K_u_inv,
 
-                   I_hx_=I_hx_,
-                   I_ux_=I_ux_,
-                   I_hz_=I_hz_,
-                   I_uz_=I_uz_,
+                   I_hx=I_hx,
+                   I_ux=I_ux,
+                   I_hz=I_hz,
+                   I_uz=I_uz,
 
                    I_zu_inv_K_u_I_uz=I_zu_inv_K_u_I_uz,
 
@@ -115,14 +117,13 @@ def elbo(c):
         scalar: ELBO.
     """
     return -0.5*c.n*B.log(2*B.pi*c.sigma**2) + \
-           -0.5/c.sigma**2*B.sum(c.y**2) + \
-           -0.5/c.sigma**2*B.sum(c.q_u.mean*B.mm(c.A_sum, c.q_u.mean)) + \
-           -0.5/c.sigma**2*B.sum(c.q_u.var*c.A_sum) + \
-           -0.5/c.sigma**2*c.c_sum + \
-           -0.5*B.logdet(c.K_z_) + \
-           0.5*2*B.sum(B.log(B.diag(c.L_inv_cov_z))) + \
-           0.5*B.sum(c.root**2) - \
-           c.q_u.kl(c.p_u)
+           -0.5/c.sigma**2*(B.sum(c.y**2) +
+                            B.sum(c.q_u.m2*c.A_sum) +
+                            c.c_sum) + \
+           -0.5*B.logdet(c.K_z) + \
+           -0.5*2*B.sum(B.log(B.diag(c.L_inv_cov_z))) + \
+           0.5*B.sum(c.root**2) + \
+           -c.q_u.kl(c.p_u)
 
 
 def predict(c):
@@ -142,14 +143,15 @@ def predict(c):
     q_z = Normal(cov_z, mu_z)
 
     # Compute mean.
-    mu = B.flatten(B.mm(B.mm(c.q_u.mean, c.I_uz_, tr_a=True), B.dense(mu_z)))
+    mu = B.flatten(B.mm(c.q_u.mean, c.I_uz, B.dense(mu_z), tr_a=True))
 
     # Compute variance.
-    A = c.I_ux_ - B.mm(B.mm(c.I_uz_, B.dense(c.K_z_inv)), c.I_uz_, tr_b=True)
-    B_ = c.I_hz_ - c.I_zu_inv_K_u_I_uz
-    c_ = c.I_hx_ - \
-         B.sum(c.K_u_inv*c.I_ux_) - \
-         B.sum(B.sum(c.K_z_inv[None, :, :]*c.I_hz_, axis=1), axis=1) + \
+    A = c.I_ux[None, :, :] - \
+        B.mm(c.I_uz, B.dense(c.K_z_inv), c.I_uz, tr_c=True)
+    B_ = c.I_hz - c.I_zu_inv_K_u_I_uz
+    c_ = c.I_hx - \
+         B.sum(c.K_u_inv*c.I_ux) - \
+         B.sum(B.sum(c.K_z_inv[None, :, :]*c.I_hz, axis=1), axis=1) + \
          B.sum(B.sum(c.K_z_inv[None, :, :]*c.I_zu_inv_K_u_I_uz, axis=1),
                axis=1)
     var = B.sum(B.sum(A*c.q_u.m2[None, :, :], axis=1), axis=1) + \
@@ -159,7 +161,7 @@ def predict(c):
 
 
 def predict_kernel(c):
-    """Predict kernel.
+    """Predict kernel and normalise prediction.
 
     Args:
         c (:class:`collections.namedtuple`): Computed quantities.
@@ -168,7 +170,7 @@ def predict_kernel(c):
         :class:`collections.namedtuple`: Named tuple containing the prediction.
     """
     # Transform back to normal space.
-    q_u_orig = c.q_u.lmatmul(c.K_u_)
+    q_u_orig = c.q_u.lmatmul(c.K_u)
 
     ks = []
     t_k = B.linspace(c.model.dtype, 0, 1.2*B.max(c.model.t_u), 100)
@@ -178,8 +180,13 @@ def predict_kernel(c):
                             t2=B.zeros(c.model.dtype, 1),
                             u=B.flatten(q_u_orig.sample()))
         ks.append(B.flatten(k))
-
     ks = B.to_numpy(B.stack(*ks, axis=0))
+
+    # Normalise predicted kernel.
+    var_mean = B.mean(ks[:, 0])
+    ks = ks/var_mean
+    wbml.out.kv('Mean variance of kernel prediction', var_mean)
+
     k_mean = B.mean(ks, axis=0)
     k_68 = (np.percentile(ks, 32, axis=0),
             np.percentile(ks, 100 - 32, axis=0))
