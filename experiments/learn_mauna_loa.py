74310e3eed888528caacebbb4ac64dcb87182b89
W.P. Bruinsma <wpb23@cblgpu11.eng.cam.ac.uk>
2021-09-29 18:07:12 +0100

    Adjust Mauna Loa experiment

--- experiments/learn_mauna_loa.py
+++ experiments/learn_mauna_loa.py
@@ -5,7 +5,7 @@ from wbml.data.mauna_loa import load
 
 args, wd = setup("mauna_loa")
 
-n = 400
+n = 200
 data = load()
 t = np.array(data.index)[-n:]
 y = np.array(data["ppm_detrended"])[-n:]
@@ -17,9 +17,9 @@ y -= B.mean(y)
 y /= B.std(y)
 
 # Setup GPCM models.
-noise = 0.1
+noise = 0.05
 window = min(max(t) / 2, 15)
-scale = 2 / 12
+scale = 3 / 12
 # Set two points per wiggle of the filter.
 n_u = int(2 * (2 * window) / scale)
 # Set two inducing points per frequency and add Nyquist correction.
