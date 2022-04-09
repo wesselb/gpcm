d8dee90d9d968861cbfffabf953eff23b32721f3
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2022-04-09 18:35:07 +0100

    Update comparison experiment

--- experiments/paper/comparison.py
+++ experiments/paper/comparison.py
@@ -1,11 +1,11 @@
 import lab as B
 import wbml.out as out
+from gpcm import GPCM, CGPCM, RGPCM
 from slugify import slugify
 from stheno import EQ, CEQ, Exp, GP, Delta
 from wbml.experiment import WorkingDirectory
 
-from gpcm import GPCM, CGPCM, RGPCM
-
+# Setup script.
 out.report_time = True
 B.epsilon = 1e-8
 wd = WorkingDirectory("_experiments", "comparison")
@@ -21,7 +21,44 @@ scale = 0.5
 n_u = 30
 n_z = 80
 
-for kernel in [CEQ(1), EQ(), Exp()]:
+for kernel, model in [
+    (
+        EQ(),
+        lambda scheme: GPCM(
+            scheme=scheme,
+            window=window,
+            scale=scale,
+            noise=noise,
+            n_u=n_u,
+            n_z=n_z,
+            t=t,
+        ),
+    ),
+    (
+        CEQ(1),
+        lambda scheme: CGPCM(
+            scheme=scheme,
+            window=window,
+            scale=scale,
+            noise=noise,
+            n_u=n_u,
+            n_z=n_z,
+            t=t,
+        ),
+    ),
+    (
+        Exp(),
+        lambda scheme: RGPCM(
+            scheme=scheme,
+            window=window,
+            scale=scale,
+            noise=noise,
+            n_u=n_u,
+            m_max=n_z // 2,
+            t=t,
+        ),
+    ),
+]:
     # Sample data.
     gp_f = GP(kernel)
     gp_y = gp_f + GP(noise * Delta(), measure=gp_f.measure)
@@ -39,46 +76,17 @@ for kernel in [CEQ(1), EQ(), Exp()]:
     )
 
     for scheme in ["mean-field", "structured"]:
-        for model in [
-            GPCM(
-                scheme=scheme,
-                window=window,
-                scale=scale,
-                noise=noise,
-                n_u=n_u,
-                n_z=n_z,
-                t=t,
-            ),
-            CGPCM(
-                scheme=scheme,
-                window=window,
-                scale=scale,
-                noise=noise,
-                n_u=n_u,
-                n_z=n_z,
-                t=t,
-            ),
-            RGPCM(
-                scheme=scheme,
-                window=window,
-                scale=scale,
-                noise=noise,
-                n_u=n_u,
-                m_max=n_z // 2,
-                t=t,
-            ),
-        ]:
-            prefix = (slugify(str(kernel)), scheme, slugify(model.name))
+        prefix = (slugify(str(kernel)), scheme, slugify(model.name))
 
-            # Fit model and predict function and kernel.
-            model.fit(t, y, iters=20_000)
-            elbo = model.elbo(t, y)
-            posterior = model.condition(t, y)
-            f_pred = posterior.predict(t)
-            k_pred = posterior.predict_kernel(t_k)
+        # Fit model and predict function and kernel.
+        model.fit(t, y, iters=20_000)
+        elbo = model.elbo(t, y)
+        posterior = model.condition(t, y)
+        f_pred = posterior.predict(t)
+        k_pred = posterior.predict_kernel(t_k)
 
-            # Save stuff.
-            model.save(wd.file(*prefix, "model.pickle"))
-            wd.save(elbo, *prefix, "elbo.pickle")
-            wd.save((t,) + f_pred, *prefix, "f_pred.pickle")
-            wd.save((k_pred.x, k_pred.mean, k_pred.var), *prefix, "k_pred.pickle")
+        # Save stuff.
+        model.save(wd.file(*prefix, "model.pickle"))
+        wd.save(elbo, *prefix, "elbo.pickle")
+        wd.save((t,) + f_pred, *prefix, "f_pred.pickle")
+        wd.save((k_pred.x, k_pred.mean, k_pred.var), *prefix, "k_pred.pickle")
